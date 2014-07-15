a7ee180ad01b352f1432c0f0749efd3bcd1788c7
chris <chris@eeepc>
2014-07-15 10:18:26 +0200

    the newest one. plus one.

--- HelloWorld/src/HelloWorld2.java
+++ HelloWorld/src/HelloWorld2.java
@@ -2,7 +2,7 @@
 public class HelloWorld2 {
 
   public static void main(String[] args) {
-    System.out.println("Called main changed");
+    System.out.println("Called main changed. Changed again");
   }
 
 }
